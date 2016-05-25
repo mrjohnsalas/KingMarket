245905dedd6a6b7d9de48afc3ce7b24762fe7d2f
ricardo333 <ricardo.h.q@hotmail.com>
2016-05-25 00:37:41 -0500

    StatusService Implementation

---
+++ KingMarket.Web/Controllers/StatusController.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using KingMarket.Model.Models;
+using KingMarket.Web.StatusService;
+using PagedList;
+
+namespace KingMarket.Web.Controllers
+{
+    // GET: Status
+    public class StatusController : Controller
+    {
+        // GET: Status
+        [Authorize(Roles = "Admin")]
+        public ActionResult Index(string sortOrder, string currentFilter, string searchString, int? page)
+        {
+            var proxy = new StatusServiceClient();
+
+            ViewBag.CurrentSort = sortOrder;
+            ViewBag.NameSortParm = String.IsNullOrEmpty(sortOrder) ? "Name_Desc" : String.Empty;
+
+            if (searchString != null)
+                page = 1;
+            else
+                searchString = currentFilter;
+
+            ViewBag.CurrentFilter = searchString;
+
+            var status = sortOrder == "Name_Desc" ?
+                String.IsNullOrEmpty(searchString) ?
+                proxy.GetStatuses().OrderByDescending(c => c.Name) :
+                proxy.GetStatuses().OrderByDescending(c => c.Name).Where(s => s.Name.Contains(searchString)) :
+                String.IsNullOrEmpty(searchString) ?
+                proxy.GetStatuses().OrderBy(c => c.Name) :
+                proxy.GetStatuses().OrderBy(c => c.Name).Where(s => s.Name.Contains(searchString));
+
+            int pageSize = 5;
+            int pageNumber = (page ?? 1);
+            return View(status.ToPagedList(pageNumber, pageSize));
+        }
+
+        // GET: Status/Details/5
+        [Authorize(Roles = "Admin")]
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            var proxy = new StatusServiceClient();
+            var status = proxy.GetStatus(id.Value);
+            if (status == null)
+                return HttpNotFound();
+            return View(status);
+        }
+
+        // GET: Status/Create
+        [Authorize(Roles = "Admin")]
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")]
+        public ActionResult Create([Bind(Include = "StatusId,Name")] Status status)
+        {
+            if (ModelState.IsValid)
+            {
+                var proxy = new StatusServiceClient();
+                proxy.CreateStatus(status);
+                return RedirectToAction("Index");
+            }
+
+            return View(status);
+        }
+
+        // GET: Status/Edit/5
+        [Authorize(Roles = "Admin")]
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            var proxy = new StatusServiceClient();
+            var status = proxy.GetStatus(id.Value);
+            if (status == null)
+                return HttpNotFound();
+            return View(status);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")]
+        public ActionResult Edit([Bind(Include = "StatusId,Name")] Status status)
+        {
+            if (ModelState.IsValid)
+            {
+                var proxy = new StatusServiceClient();
+                proxy.EditStatus(status);
+                return RedirectToAction("Index");
+            }
+            return View(status);
+        }
+
+        // GET: Status/Delete/5
+        [Authorize(Roles = "Admin")]
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            var proxy = new StatusServiceClient();
+            var status = proxy.GetStatus(id.Value);
+            if (status == null)
+                return HttpNotFound();
+            return View(status);
+        }
+
+        // POST: Status/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        [Authorize(Roles = "Admin")]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            var proxy = new StatusServiceClient();
+            proxy.DeleteStatus(id);
+            return RedirectToAction("Index");
+        }
+    }
+}
